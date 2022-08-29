e2559b3bba7c5c2393f5a8e8334b6149843a5d98
Bakenneko <andronniy7@gmail.com>
2022-08-29 23:39:38 +0300

    Test

---
+++ Lesson10/2..js
@@ -0,0 +1,11 @@
+// 2. Вивести id,name всіх user в index.html. Окремий блок для кожного user.
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(users => {
+//         for (const user of users) {
+//             let div = document.createElement('div');
+//             div.innerText = `${user.id} - ${user.name}`;
+//             document.body.appendChild(div);
+//         }
+//     });
